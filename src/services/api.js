6f1a8f3dd883db7422eff5913be33e27134a951e
Varshith Reddy <2100031679cseh@gmail.com>
2025-09-15 02:03:07 +0530

    mycommit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://notes-saas-app-wbtt.vercel.app/api';
 
 export function setAuthToken(token) {
   if (token) {
